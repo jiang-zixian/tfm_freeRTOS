f4e0679759f65ea0ac8c7d56502f509901113a40
jiang-zixian <980485445@qq.com>
2024-04-09 22:25:23 +0800

    trampoline can not run before r9Init

--- platform/ext/target/stm/aslr_freertos_nsapp/loader/src/divide.c
+++ platform/ext/target/stm/aslr_freertos_nsapp/loader/src/divide.c
@@ -7,22 +7,10 @@ void divide() {
         if (((func_info[i].addr >> 1) & 1) == 1) {
             func_info[i].region = 1;
         }
-        // // if(func_info[i].addr==0x8058661/*SVC_Handler*/||
-        // // func_info[i].addr==0x805876d/*memset*/||
-        // // func_info[i].addr==0x8058611/*PendSV_Handler*/||
-        // // func_info[i].addr==0x8055c31/*prvCreateTask*/||
-        // // func_info[i].addr==0x8056ae1/*prvInitialiseNewTask*/||
-        // // func_info[i].addr==0x80585cb/*vStartFirstTask*/){
-        // //     func_info[i].region = 0;
-        // // }
-        // if(func_info[i].addr==0x8058451/*SVC_Handler*/||
-        // func_info[i].addr==0x805855d/*memset*/||
-        // func_info[i].addr==0x8058401/*PendSV_Handler*/||
-        // func_info[i].addr==0x8055a1d/*prvCreateTask*/||
-        // func_info[i].addr==0x80568cd/*prvInitialiseNewTask*/||
-        // func_info[i].addr==0x80583b7/*vStartFirstTask*/){
-        //     func_info[i].region = 0;
-        // }
+        //要保证ns世界的reset_handler和r9Init两个函数在同一个区域,因为r9Init之前不能用trampoline
+        if(func_info[i].addr==0x805586b||func_info[i].addr==0x8055781){
+            func_info[i].region = 0;
+        }
         
     }
 }
